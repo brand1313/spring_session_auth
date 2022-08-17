0e41b05763fce03d7c0f4f29e498b0f7e46e8f7d
brand1313 <brand13@naver.com>
2022-08-18 08:40:05 +0900

    Edit Security Config

--- src/main/java/com/study/boot/session/config/SecurityConfig.java
+++ src/main/java/com/study/boot/session/config/SecurityConfig.java
@@ -7,16 +7,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder encodePwd() { return new BCryptPasswordEncoder(); }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
         http.authorizeRequests()
@@ -24,16 +25,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll()
+                .and().headers().frameOptions().sameOrigin()
                 .and()
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/");
 
+
 //        http.sessionManagement()
 //                .maximumSessions(1) // 최대 허용 가능 세션 수, -1 : 무제한 로그인 세션 허용
 //                .maxSessionsPreventsLogin(true) // 동시 로그인 차단, false : 기존 세션 만료(defualt)
 //                .expiredUrl("/"); // 세션이 만료된 경우 이동할 URL
+
+        return http.build();
     }
 
 
